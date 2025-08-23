104f7d3915c5dfa7412416f3d4ccb85e2251107e
Vitamin D <85170824+Vitamins-D@users.noreply.github.com>
2025-08-23 15:33:09 +0200

    fix 2

--- scripts/vscripts/utils/wavesAddon.lua
+++ scripts/vscripts/utils/wavesAddon.lua
@@ -64,6 +64,106 @@ function wa:InitAddon(player, spawnPos, path, team)
 	player.team = team
 end
 
+function wa:spawnUnit(spawn_unit, player, spawnPos, pUpgrades, team)
+	local name = spawn_unit
+			
+	local unitUpg = pUpgrades[name]
+	local class = unitUpg[4]
+	local subclass = unitUpg[5]
+	
+	local pathName = name
+	if subclass then pathName = subclass
+	elseif class then pathName = class end
+	local path = info:getUnitByName(pathName)
+	
+	local unit = CreateUnitByName(path, spawnPos, true, nil, nil, team)
+	unit.path = player.path
+	unit.type = name
+	unit.class = class
+	unit.subclass = subclass
+	
+	unit.skills = {}
+	unit.bonus = {}
+	
+	local names = {name, class, subclass}
+	for i = 1, #names do
+		local pUpgrade = unitUpg[i]
+		local type = pUpgrade.type
+		local levels = pUpgrade.levels
+		local unitType = names[i]
+		
+		local skills = wi.skills[unitType]
+		if skills then
+			for k = 1, #skills do
+				local skill = skills[k]
+				utils:addAbility(unit, skill)
+			end
+		end
+		
+		for j = 1, #levels do
+			local lvl = levels[j]
+			
+			if lvl > 0 and unitType then
+				local upgrades = info.upgrades[type][unitType][j].levels
+				
+				for k = 1, lvl do
+					local upgradeLevel = upgrades[k]
+					
+					for n = 1, #upgradeLevel do
+						local upgrade = upgradeLevel[n]
+						if upgrade.type == "spell" then
+							-- print("SPELL:", upgrade.value)
+							utils:addAbility(unit, upgrade.value)
+							-- unit:AddAbility(upgrade.value)
+						elseif upgrade.type == "spell_up" then
+							utils:upgradeAbility(unit, upgrade.value)
+						elseif upgrade.type == "replace" then
+							utils:replaceAbility(unit, upgrade.value[1], upgrade.value[2])
+						else
+							if upgrade.type == "armor" then
+								unit:SetPhysicalArmorBaseValue(unit:GetPhysicalArmorBaseValue() + upgrade.value)
+							elseif upgrade.type == "magr" then
+								unit:SetBaseMagicalResistanceValue(unit:GetBaseMagicalResistanceValue() + upgrade.value)
+							elseif upgrade.type == "hpreg" then
+								unit:SetBaseHealthRegen(unit:GetBaseHealthRegen() + upgrade.value)
+							else
+								-- остальные бонусы в модификатор
+								if not unit.bonus[upgrade.type] then unit.bonus[upgrade.type] = 0 end
+								unit.bonus[upgrade.type] = unit.bonus[upgrade.type] + upgrade.value
+							end
+						end
+					end
+					
+				end
+			end
+			
+		end
+	end
+	
+	local special = info.specials[pathName]
+	if special then
+		for j = 1, #special do
+			local arr = special[j]
+			if arr[1] == "replace" then
+				utils:replaceAbility(unit, arr[2], arr[3])
+			elseif arr[1] == "set" then
+				local ability = unit:FindAbilityByName(arr[2])
+				if ability then
+					ability:SetLevel(arr[3])
+				end
+			elseif arr[1] == "upgrade" then
+				utils:upgradeAbility(unit, arr[2])
+			elseif arr[1] == "remove" then
+				utils:removeAbility(unit, arr[2])
+			end
+		end
+	end
+	
+	Timers:CreateTimer(0.5, function()
+		unit:AddNewModifier(unit, nil, "modifier_buff_stats", {})
+	end)
+end
+
 function wa:spawnWave(player)
 	local spawnPos = player.spawnPos
 	local units = player.units
@@ -81,103 +181,22 @@ function wa:spawnWave(player)
 	
 	for i = 1, #units do
 		Timers:CreateTimer(0.5*i, function()
-			local name = units[i]
-			
-			local unitUpg = pUpgrades[name]
-			local class = unitUpg[4]
-			local subclass = unitUpg[5]
-			
-			local pathName = name
-			if subclass then pathName = subclass
-			elseif class then pathName = class end
-			local path = info:getUnitByName(pathName)
-			
-			local unit = CreateUnitByName(path, spawnPos, true, nil, nil, team)
-			unit.path = player.path
-			unit.type = name
-			unit.class = class
-			unit.subclass = subclass
-			
-			unit.skills = {}
-			unit.bonus = {}
-			
-			local names = {name, class, subclass}
-			for i = 1, #names do
-				local pUpgrade = unitUpg[i]
-				local type = pUpgrade.type
-				local levels = pUpgrade.levels
-				local unitType = names[i]
-				
-				local skills = wi.skills[unitType]
-				if skills then
-					for k = 1, #skills do
-						local skill = skills[k]
-						utils:addAbility(unit, skill)
-					end
-				end
-				
-				for j = 1, #levels do
-					local lvl = levels[j]
-					
-					if lvl > 0 and unitType then
-						local upgrades = info.upgrades[type][unitType][j].levels
-						
-						for k = 1, lvl do
-							local upgradeLevel = upgrades[k]
-							
-							for n = 1, #upgradeLevel do
-								local upgrade = upgradeLevel[n]
-								if upgrade.type == "spell" then
-									-- print("SPELL:", upgrade.value)
-									utils:addAbility(unit, upgrade.value)
-									-- unit:AddAbility(upgrade.value)
-								elseif upgrade.type == "spell_up" then
-									utils:upgradeAbility(unit, upgrade.value)
-								elseif upgrade.type == "replace" then
-									utils:replaceAbility(unit, upgrade.value[1], upgrade.value[2])
-								else
-									if upgrade.type == "armor" then
-										unit:SetPhysicalArmorBaseValue(unit:GetPhysicalArmorBaseValue() + upgrade.value)
-									elseif upgrade.type == "magr" then
-										unit:SetBaseMagicalResistanceValue(unit:GetBaseMagicalResistanceValue() + upgrade.value)
-									elseif upgrade.type == "hpreg" then
-										unit:SetBaseHealthRegen(unit:GetBaseHealthRegen() + upgrade.value)
-									else
-										-- остальные бонусы в модификатор
-										if not unit.bonus[upgrade.type] then unit.bonus[upgrade.type] = 0 end
-										unit.bonus[upgrade.type] = unit.bonus[upgrade.type] + upgrade.value
-									end
-								end
-							end
-							
-						end
-					end
-					
-				end
-			end
-			
-			local special = info.specials[pathName]
-			if special then
-				for j = 1, #special do
-					local arr = special[j]
-					if arr[1] == "replace" then
-						utils:replaceAbility(unit, arr[2], arr[3])
-					elseif arr[1] == "set" then
-						local ability = unit:FindAbilityByName(arr[2])
-						if ability then
-							ability:SetLevel(arr[3])
-						end
-					elseif arr[1] == "upgrade" then
-						utils:upgradeAbility(unit, arr[2])
-					elseif arr[1] == "remove" then
-						utils:removeAbility(unit, arr[2])
-					end
-				end
-			end
-			
-			Timers:CreateTimer(0.5, function()
-				unit:AddNewModifier(unit, nil, "modifier_buff_stats", {})
-			end)
+			wa:spawnUnit(units[i], player, spawnPos, pUpgrades, team)
+		end)
+	end
+end
+
+function wa:spawnCaravan(player)
+	local spawnPos = player.spawnPos
+	local units = player.units
+	local pUpgrades = player.upgrades
+	local team = player.team
+	
+	units = wa:sortUnits(units)
+	
+	for i = 1, #units do
+		Timers:CreateTimer(0.5*i, function()
+			wa:spawnUnit(units[i], spawnPos, pUpgrades, team)
 		end)
 	end
 end
